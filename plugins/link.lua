664572b6b0935b009db1f41f03b57f9cd2a2a5e8
AliNiestani <aliniestani@gmail.com>
2016-03-03 21:56:21 +0330

    Create link.lua

---
+++ plugins/link.lua
@@ -0,0 +1,15 @@
+do
+     
+function run(msg, matches)
+return 'سلام'
+
+
+end
+
+  return {
+  description = "A",
+  usage = {"دلام", "سلام", "شلام"},
+  patterns = {"^دلام","^سلام","^شلام"}, 
+  run = run 
+}
+end
